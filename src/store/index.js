7a42d9047ea5732a75d457f910d0ffe43187fbbe
peeea4 <nikita.gabetc@gmail.com>
2022-09-02 22:56:14 +0300

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
+
+import { rootReducer } from "./reducers";
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
---
+++ src/styles/index.js
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+* {
+
+    margin: 0;
+    padding: 0;
+
+    box-sizing: border-box;
+    
+    font-family: "Montserrat";
+
+    ::-webkit-scrollbar {
+        width: 6px;
+        height: 6px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: rgba(158, 158, 158, 0.5);
+        border-radius: 8px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: rgba(69, 69, 69, 0.7);
+        border-radius: 8px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: rgba(69, 69, 69, 1);
+    }
+}
+
+html, body {
+    width: 100%;
+    height: 100%;
+}
+
+body {  
+    & > #root {
+        width: 100%;
+        height: 100%;
+    }
+}
+
+body {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+h1 {
+    padding: 0;
+    margin: 0;
+}
+
+#modal-root {
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+`;
